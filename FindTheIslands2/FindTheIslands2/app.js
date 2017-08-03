9cb9a2d3e2009c111dc4f6228104042c49a157fc
CodyWahl <codywahl@gmail.com>
2017-08-03 13:11:01 -0700

    Finished implementation
    It works!

--- FindTheIslands2/FindTheIslands2/app.js
+++ FindTheIslands2/FindTheIslands2/app.js
@@ -1,17 +1,48 @@
-var islands = [[0, 1, 1],
-    [1, 0, 1],
-    [1, 0, 0]];
+var islands = [[0, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 1],
+    [1, 0, 1, 1, 0, 0],
+    [1, 0, 0, 1, 1, 0],
+    [0, 1, 1, 0, 1, 0]];
 var islandCount = 0;
-console.log(checkIfTopLeftOfIsland(1, 2));
 for (var i = 0; i < islands.length; i++) {
     var currentCollection = islands[i];
-    var currentRow = '';
+    var currentRow = "";
     for (var j = 0; j < currentCollection.length; j++) {
         currentRow = currentRow + " " + currentCollection[j];
+        if (checkIfTopLeftOfIsland(i, j)) {
+            islandCount++;
+        }
     }
     console.log(currentRow);
 }
+console.log("Island count: " + islandCount);
+function isAnIsland(x, y) {
+    return islands[x][y] === 1;
+}
 function checkIfTopLeftOfIsland(x, y) {
-    return x + y;
+    if (x < 0 || y < 0) {
+        throw new TypeError("X and Y values must be greater than 0.");
+    }
+    if (!isAnIsland(x, y)) {
+        return false;
+    }
+    if (x === 0 && y === 0) {
+        return true;
+    }
+    // Check if there is an island above
+    if (y > 0) {
+        var islandAbove = isAnIsland(x, y - 1);
+        if (islandAbove) {
+            return false;
+        }
+    }
+    // Check if there is an island left
+    if (x > 0) {
+        var islandLeft = isAnIsland(x - 1, y);
+        if (islandLeft) {
+            return false;
+        }
+    }
+    return true;
 }
 //# sourceMappingURL=app.js.map
--- FindTheIslands2/FindTheIslands2/app.ts
+++ FindTheIslands2/FindTheIslands2/app.ts
@@ -1,38 +1,63 @@
-﻿let islands = [[0, 1, 1],
-               [1, 0, 1], 
-               [1, 0, 0]];
+﻿var islands = [[0, 1, 1, 1, 1, 1],
+               [1, 0, 0, 0, 1, 1], 
+               [1, 0, 1, 1, 0, 0],
+               [1, 0, 0, 1, 1, 0],
+               [0, 1, 1, 0, 1, 0]];
 
 let islandCount = 0;
 
 for (let i = 0; i < islands.length; i++) {
     let currentCollection = islands[i];
-    let currentRow = '';
+    let currentRow = "";
 
     for (let j = 0; j < currentCollection.length; j++) {
         currentRow = currentRow + " " + currentCollection[j];
+
+        if (checkIfTopLeftOfIsland(i, j)) {
+            islandCount++;
+        }
     }
 
     console.log(currentRow);
 }
 
+console.log(`Island count: ${islandCount}`);
+
 function isAnIsland(x: number, y: number) {
-    return islands[x][y] == 1;
+    return islands[x][y] === 1;
 }
 
 function checkIfTopLeftOfIsland(x: number, y: number) {
-    let isTopLeftOfIsland = false;
-    let isLeftOfIsland = false;
-    let isTopOfIsland = false;
+    if (x < 0 || y < 0) {
+        throw new TypeError("X and Y values must be greater than 0.");
+    }
 
-    if (isAnIsland(x, y) && x === 0 && y === 0) {
+    if (!isAnIsland(x, y)) {
+        return false;
+    }
+
+    if (x === 0 && y === 0) {
         return true;
     }
 
-    if (x != 0 && y != 0) {
+    // Check if there is an island above
+    if (y > 0) {
+        let islandAbove = isAnIsland(x, y - 1);
 
+        if (islandAbove) {
+            return false;
+        }
     }
 
-    if (islandCount[x][y])
-    return islands[x][y] == 1 &&
-           
+    // Check if there is an island left
+    if (x > 0) {
+        let islandLeft = isAnIsland(x - 1, y);
+
+        if (islandLeft) {
+            return false;
+        }
+    }
+
+    return true;
+
 }
